6047556877e083d6a619b00c00fa76eff41c943e
faqihyugos <faqihyugos@gmail.com>
2021-01-19 12:29:54 +0700

    function lanjutan

--- function/main.go
+++ function/main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	sentence := printMyResult("Saya sedang")
-	fmt.Println(sentence)
-
+	// sentence := printMyResult("Saya sedang")
+	// fmt.Println(sentence)
+	result := add(20, 10)
+	fmt.Println(result)
 }
 
-func printMyResult(sentence string) string {
-	newSentence := sentence + " Belajar Golang"
-	return newSentence
+func add(number, numberTwo int) int {
+	return number + numberTwo
 }
+
+// func printMyResult(sentence string) string {
+// 	newSentence := sentence + " Belajar Golang"
+// 	return newSentence
+// }
+
+// 1. input
+// 2. proses
+// 3. output
