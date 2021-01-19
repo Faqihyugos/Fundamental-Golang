b427caf41022c71cda12ea58fa70e8f5abee3d99
faqihyugos <faqihyugos@gmail.com>
2021-01-19 12:36:43 +0700

    function multiple return

--- function/main.go
+++ function/main.go
@@ -5,13 +5,26 @@ import "fmt"
 func main() {
 	// sentence := printMyResult("Saya sedang")
 	// fmt.Println(sentence)
-	result := add(20, 10)
-	fmt.Println(result)
+
+	// result := add(20, 10)
+	// fmt.Println(result)
+
+	luas, keliling := calculate(10, 2)
+	fmt.Println("Luas : ", luas)
+	fmt.Println("Keliling : ", keliling)
+
 }
 
-func add(number, numberTwo int) int {
-	return number + numberTwo
+func calculate(panjang, lebar int) (int, int) {
+	luas := panjang * lebar
+	keliling := 2 * (panjang + lebar)
+
+	return luas, keliling
 }
+
+// func add(number, numberTwo int) int {
+// 	return number + numberTwo
+// }
 
 // func printMyResult(sentence string) string {
 // 	newSentence := sentence + " Belajar Golang"
